272867646a4ac9e3166bb7ba49bbaed52ac2a7dc
asad <asadhamasy@gmail.com>
2023-09-20 08:36:28 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+  );
+};
+
+export default App;
